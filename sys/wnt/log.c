7bb92514fb458eb28c0a9d76a11511739691a744
Tristan `Aversiste' Le Guern <tristan.leguern@gmail.com>
2012-01-04 10:05:42 +0100

    Push the logging API
    Original work from Henning Brauer <henning@openbsd.org>, with some
    minor modification.

---
+++ sys/wnt/log.c
@@ -0,0 +1,36 @@
+/* Here will be the Windows NT logging API implementation */
+
+#include "log.h"
+
+void
+log_init(void) {
+}
+
+void
+log_err(int eval, const char *, ...) {
+}
+
+void
+log_errx(int eval, const char *) {
+}
+
+void
+log_warn(const char *, ...) {
+}
+
+void
+log_warnx(const char *, ...) {
+}
+
+void
+log_notice(const char *, ...) {
+}
+
+void
+log_info(const char *, ...) {
+}
+
+void
+log_debug(const char *, ...) {
+}
+
