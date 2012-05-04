17e55bcd5aa0b4a06633e88337761688d7e36c2b
Antoine Marandon <ntnmrndn@gmail.com>
2012-05-04 19:01:34 +0900

    Refactoring compression to comply with documentation.

--- src/compress.c
+++ src/compress.c
@@ -27,16 +27,15 @@
 #include "compress.h"
 #include "log.h"
 
-t_string *
-tnt_compress(t_string *in)
+uchar *
+tnt_compress(uchar *in, const size_t in_size, size_t *out_size)
 {
     int		error;
-    t_string	*out;
+    uchar	*out;
     z_stream	strm;
 
-    out = (t_string *)malloc(sizeof(*out));
-    out->data = (unsigned char *)malloc((in->size < Z_MIN_SPACE ? Z_MIN_SPACE : in->size));
-    if (out == NULL || out->data == NULL)
+    out = (unsigned char *)malloc((in_size < Z_MIN_SPACE ? Z_MIN_SPACE : in_size));
+    if (out == NULL)
     {
         log_warn("do_compress");
         return NULL;
@@ -52,10 +51,10 @@ tnt_compress(t_string *in)
         log_warnx("zlib: error on deflateInit (%d)\n", error);
         return (NULL);
     }
-    strm.avail_in = in->size;
-    strm.next_in = (Bytef *)in->data;
-    strm.avail_out = (in->size < Z_MIN_SPACE ? Z_MIN_SPACE : in->size);
-    strm.next_out = (Bytef *)out->data;
+    strm.avail_in = in_size;
+    strm.next_in = (Bytef *)in;
+    strm.avail_out = (in_size < Z_MIN_SPACE ? Z_MIN_SPACE : in_size);
+    strm.next_out = (Bytef *)out;
 
     /* Compression */
     do
@@ -65,17 +64,17 @@ tnt_compress(t_string *in)
             break;
 
 	/* In this case, the compressed version is bigger */
-	out->data = (unsigned char *)realloc(out->data, strm.total_out +
-	  (in->size / 2 < Z_MIN_SPACE ? Z_MIN_SPACE : in->size / 2));
-        if (out->data == NULL)
+	out = (unsigned char *)realloc(out, strm.total_out +
+	  (in_size / 2 < Z_MIN_SPACE ? Z_MIN_SPACE : in_size / 2));
+        if (out == NULL)
         {
             log_warn("do_compress");
             (void)deflateEnd(&strm);
             return NULL;
         }
-        strm.next_out = (Bytef *)out->data + strm.total_out;
-	if (in->size / 2 < Z_MIN_SPACE)
-            strm.avail_out = in->size / 2;
+        strm.next_out = (Bytef *)out + strm.total_out;
+	if (in_size / 2 < Z_MIN_SPACE)
+            strm.avail_out = in_size / 2;
         else
             strm.avail_out = Z_MIN_SPACE;
     } while (error == Z_OK);
@@ -84,28 +83,25 @@ tnt_compress(t_string *in)
     if (error != Z_STREAM_END)
     {
         log_warnx("zlib: deflating error\n");
-        free(out->data);
         free(out);
         (void)deflateEnd(&strm);
         return NULL;
     }
-    out->size = strm.total_out;
+    *out_size = strm.total_out;
     (void)deflateEnd(&strm);
     /* XXX: We should realloc here, adjusting the size */
     return out;
 }
 
-t_string *
-tnt_uncompress(t_string *in, const size_t orignal_len)
+uchar *
+tnt_uncompress(uchar *in, const size_t in_size, const size_t orignal_len)
 {
-	int error;
-    int		ret;
-    t_string	*out;
-    z_stream	strm;
+    int          ret;
+    uchar       *out;
+    z_stream	 strm;
 
-    out = (t_string *)malloc(sizeof(*out));
-    out->data = (unsigned char *)malloc(orignal_len);
-    if (out == NULL || out->data == NULL)
+    out = (uchar *)malloc(orignal_len);
+    if (out == NULL)
     {
         log_warn("do_uncompress");
         return NULL;
@@ -113,74 +109,23 @@ tnt_uncompress(t_string *in, const size_t orignal_len)
     strm.zalloc = Z_NULL;
     strm.zfree = Z_NULL;
     strm.opaque = Z_NULL;
-    strm.next_in = in->data;
-    strm.avail_in = in->size;
-    strm.next_out = out->data;
+    strm.next_in = in;
+    strm.avail_in = in_size;
+    strm.next_out = out;
     strm.avail_out = orignal_len;
-    out->size = orignal_len;
     ret = inflateInit(&strm);
     if (ret != Z_OK)
     {
-        log_warnx("zlib: error on inflateInit (%d)\n", error);
+        log_warnx("zlib: error on inflateInit (%d)\n", ret);
         return NULL;
     }
     ret = inflate(&strm, Z_FINISH);
     if (ret != Z_STREAM_END)
     {
-        log_warnx("zlib: error on inflate (%d)\n", error);
-        free(out->data);
+        log_warnx("zlib: error on inflate (%d)\n", ret);
         free(out);
         out = NULL;
     }
     (void)inflateEnd(&strm);
     return out;
 }
-//
-//int	main(int ac, const char **av)
-//{
-//  HANDLE	fd;
-//  HANDLE map;
-//  int tmp;
-//  t_string original;
-//  t_string *compressed;
-//  t_string *uncompressed;
-//
-//  if (!av[1])
-//    {
-//      log_warn("Usage: ./a.out filename\n");
-//      return (EXIT_FAILURE);
-//    }
-//	fd = CreateFile(av[1], GENERIC_READ,0,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
-//  if (fd == INVALID_HANDLE_VALUE)
-//    {
-//      log_warn("Pas de fichier\n");
-//      return (-1);
-//    }
-//  original.size = GetFileSize(fd, (LPDWORD)(&tmp));
-////  lseek(fd, 0, SEEK_SET);
-//  map = CreateFileMapping(fd, NULL, PAGE_READWRITE, 0, 0, "filemap");
-//  // mmap == mapViewOfFile()
-//  original.data = (unsigned char *)MapViewOfFile(map, FILE_MAP_ALL_ACCESS, 0, 0, original.size);//mmap(NULL, original.size, PROT_READ, MAP_PRIVATE, fd, 0);
-//  CloseHandle(fd);
-//
-//  compressed = tnt_compress(&original); // haaaan il verifie pas la valeur de retour!
-//  if (compressed == NULL)
-//	  printf("mais bordel !\n");
-//  printf("Orginal size was %lu ... new one is %lu\n", original.size, compressed->size);
-//  uncompressed = tnt_uncompress(compressed, original.size);
-//  printf("Orginal size was %lu ... restored size is: %lu difference is %lu\n",
-//	 original.size, uncompressed->size, original.size - uncompressed->size);
-//    if (original.size == uncompressed->size)
-//    printf(memcmp(original.data, uncompressed->data, original.size) ?
-//		  "Diff KO\n": "Diff OK\n");
-//
-//	UnmapViewOfFile(original.data);
-//	CloseHandle(map);
-//	//	munmap(original.data, original.size);
-//  printf("\nRatio is %lf%%\n", ((float)(original.size - compressed->size)) / original.size * 100);
-//  free(compressed->data);
-//  free(compressed);
-//  free(uncompressed->data);
-//  free(uncompressed);
-//  return (EXIT_SUCCESS);
-//}
