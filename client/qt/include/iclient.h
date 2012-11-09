b058b5de9d9f11bc2ce091fd9a18240df640dafd
tribou_f <florent.tribouilloy@epitech.net>
2012-11-09 17:59:53 +0100

    move iclientgui into iclient

--- client/qt/include/iclient.h
+++ client/qt/include/iclient.h
@@ -1,17 +1,15 @@
-#ifndef ICLIENTGUI_H
-#define ICLIENTGUI_H
+#ifndef ICLIENT_H
+#define ICLIENT_H
 
 #include <QString>
 
-class IClientGUI
+class IClient
 {
 public :
     virtual QWidget*    getHeader() const = 0;
     virtual QWidget*    getBody() const = 0;
-    virtual void        changeNextBody() = 0;
-    virtual void        changePrevBody() = 0;
     virtual void        connected() = 0;
     virtual void        disconnected() = 0;
 };
 
-#endif // ICLIENTGUI_H
+#endif // ICLIENT_H
